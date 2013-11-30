12230b98de3632c8d5d6eaf0ffde7df53e855048
Kevin Middleton <middletonk@missouri.edu>
2013-11-29 20:49:48 -0600

    Add glosso data.

--- R/datasets.R
+++ R/datasets.R
@@ -15,3 +15,26 @@
 ##' @keywords package
 ##' 
 NULL
+
+
+##' Glossophaga Radius
+##' 
+##' Glossophaga radius in cantilever bending. Scale: 107 pixels = 1
+##' cm.
+##' 
+##' @name glosso
+##' 
+##' @docType data
+##' 
+##' @format A \code{data.frame} consisting of:
+##' \tabular{ll}{ Trial: \tab Trial number\cr 
+##'               P: \tab Load (N)\cr 
+##'               L: \tab Bone length\cr 
+##'               x_pix: \tab x position in pixels\cr 
+##'               u_pix: \tab x position in pixels}
+##' 
+##' @source Recorded by Kevin Middleton.
+##' 
+##' @keywords datasets
+##' 
+NULL
